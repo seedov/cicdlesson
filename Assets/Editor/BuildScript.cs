b4f7e124c4f681bed3da8950e46bbc9a6ead098f
antonsedov <antonsedov>
2026-05-15 20:10:31 +0300

    add bundle

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEditor;
+
+public static class BuildScript
+{
+    [MenuItem("Create/Create Bundle")]
+    public static void BuildAssetBundles()
+    {
+        Debug.Log("BuildAssetBundles");
+    }
+}
